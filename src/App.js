04fa296edd3ec237e5c976549e447d3f50b50a6a
AabhasGaur19 <aabhasgaur14@gmail.com>
2025-09-21 09:25:48 +0530

    changed for travis

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react ,ok,ok
+          Learn react ,ok ma
         </a>
       </header>
     </div>
